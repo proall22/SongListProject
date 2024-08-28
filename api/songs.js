72af66f759a30bed1d58d4000819c43a24f239c8
proall22 <allaboutpro22@gmail.com>
2024-08-28 16:26:42 +0300

    song project

--- api/songs.js
+++ api/songs.js
@@ -1,11 +1,3 @@
-import { create, router as createRouter, defaults } from "json-server";
-import path from "path";
-
-const server = create();
-const router = createRouter(path.join(__dirname, "db.json")); // Make sure the path to db.json is correct
-const middlewares = defaults();
-
-server.use(middlewares);
-server.use(router);
-
-export default server;
+module.exports = (req, res) => {
+	res.status(200).json({ message: "API is working" });
+};
